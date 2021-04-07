38e40630212c4afc6722c09ca35a5f3a856d20c4
Rajesh Yadav <ykrajesh@outlook.com>
2021-04-07 11:18:08 +0530

    main file modify

--- deploye/main.tf
+++ deploye/main.tf
@@ -17,3 +17,6 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+resource "azurerm_virtual_machine" "Testmv" {
+  
+}
